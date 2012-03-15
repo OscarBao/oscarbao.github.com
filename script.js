9fbb8dc9f29415b075afe96c838bde70a21fc9e2
obao <OBao.bavc@gmail.com>
2012-03-14 17:08:41 -0700

    Too lazy to make a commit name!
    I promise to make a commit name next commit...

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function writeRandom(identifier) {
 	var spit = ["Hello World!", "Foo", "Bar", "Whee!"];
 	document.getElementById(identifier).innerHTML = spit[Math.floor(Math.random()*spit.length)];
 }
+
+var num = 0;
+	
+function clickWarn(identifier) {
+	num += 1;
+	
+	var spit = "Hey, you've clicked me " + num + " times already! Stop it!"
+	
+	if(num > 10) {
+		document.getElementById(identifier).innerHTML = spit;
+	}
+}
